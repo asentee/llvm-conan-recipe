8ff8dadd022e3d402fe4d3132523179fd7bd311a
AsNT <181467043+AbubakkarSiddique1930@users.noreply.github.com>
2025-12-26 15:37:51 +0600

    [test] add: test LLVM enabled project clang, if enabled

--- test/test.cpp
+++ test/test.cpp
@@ -1,6 +1,12 @@
+#include <iostream>
+
 #include <llvm/IR/LLVMContext.h>
 #include <llvm/IR/Module.h>
 #include <llvm/Support/raw_ostream.h>
+
+#ifdef LLVM_PROJECT_CLANG_ENABLED
+    #include <clang-c/Index.h>
+#endif // LLVM_PROJECT_CLANG_ENABLED
 
 int main() {
     llvm::LLVMContext context;
@@ -10,6 +16,27 @@ int main() {
 
     llvm::outs() << "LLVM Installation Check:\n";
     module->print(llvm::outs(), nullptr);
+
+    #ifdef LLVM_PROJECT_CLANG_ENABLED
+        // Test libclang
+        std::cout << "Testing enabled LLVM project: Clang\n";
+        auto index = clang_createIndex(0, 0);
+        auto unit = clang_parseTranslationUnit(
+            index,
+            "test_package.cpp",
+            nullptr,
+            0,
+            nullptr,
+            0,
+            CXTranslationUnit_None
+        );
+
+        if(unit == nullptr) {
+            std::cerr << "Failed to parse translation unit. Quitting...\n";
+            return -1;
+        }
+        auto cursor = clang_getTranslationUnitCursor(unit);
+    #endif // LLVM_PROJECT_CLANG_ENABLED
 
     return 0;
 }
